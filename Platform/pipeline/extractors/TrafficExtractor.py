efee2c45e23fba807968c2fc729569c3aba76723
luke-github <118205955+luke-github@users.noreply.github.com>
2025-10-30 20:18:10 -0400

    change user-agent to properly represent the stinger delay project

--- Platform/pipeline/extractors/TrafficExtractor.py
+++ Platform/pipeline/extractors/TrafficExtractor.py
@@ -10,7 +10,7 @@ class TrafficExtractor(BaseExtractor):
         headers = {
             'Accept': "application/json, text/plain, */*",
             'Accept-Language': "en-US,en;q=0.9",
-            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+            'User-Agent': "StingerDelay/1.0 (contact: gtstingerdelay@gmail.com)",
         }
 
         # circle around campus encompassing all bus routes *except emory route
